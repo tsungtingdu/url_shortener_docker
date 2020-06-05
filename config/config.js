2bdc3434069991264c207aec282f4a1f953028f4
tsungtingdu <tsungtingdu@gmail.com>
2020-06-05 14:34:25 +0800

    setup mysql database

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "password",
+    database: process.env.MYSQL_DATABASE || "database",
+    host: process.env.MYSQL_HOST || "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "password",
+    database: process.env.MYSQL_DATABASE || "database",
+    host: process.env.MYSQL_HOST || "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+}
