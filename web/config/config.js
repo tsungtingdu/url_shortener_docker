4b353340158a07bf8b1c644da1d32c711ecf6577
tsungtingdu <tsungtingdu@gmail.com>
2020-06-10 16:31:42 +0800

    update database name

--- web/config/config.js
+++ web/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || "password",
-    database: process.env.MYSQL_DATABASE || "database",
+    database: process.env.MYSQL_DATABASE || "td_url_db",
     host: process.env.MYSQL_HOST || "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
